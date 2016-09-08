1ac079d26137b612ae8f072d5fe82aba1ed3f8e5
Alpesh Gajbe <alpesh@tiss.edu>
2016-09-08 19:09:18 +0530

    cleanup

--- unplatform/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-import os
-import celery
-from celery import fixups
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unplatform.settings")
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from unplatform.celeryapp import app as celery_app
